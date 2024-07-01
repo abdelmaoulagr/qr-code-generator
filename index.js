90314fc53725693da5dab054f275c0416483d3bb
Abdelmaoula El Guarai <abdelmola667@gmail.com>
2024-07-01 18:23:35 -0400

    Added an endpoint for Base64

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = 3000;
 
 app.use(express.json());
 
+// This endpoint implement QR Code Generation and Storage it  as PNG
 app.post('/generate-png', async (req, res) => {
     const { text } = req.body;
     if (!text) {
@@ -23,7 +24,20 @@ app.post('/generate-png', async (req, res) => {
     }
 });
 
+// This endpoint implement QR Code Generation and Sharing it  as Base64
+app.post('/generate-base64', async (req, res) => {
+    const { text } = req.body;
+    if (!text) {
+        return res.status(400).send('Text is required');
+    }
 
+    try {
+        const base64Data = await QRCode.toDataURL(text);
+        res.status(200).send({ base64Data });
+    } catch (error) {
+        res.status(500).send('Error generating QR code');
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
---
+++ readme.md
@@ -0,0 +1,6 @@
+# QR Code Generator API
+
+This project is a Node.js and Express-based web service that provides two main functionalities for generating QR codes:
+
+1. **Generate QR Code as PNG:**  An API endpoint that takes a text input, generates a QR code from it, saves the QR code as a PNG image on the server, and returns the file path.
+2. **Generate QR Code as Base64:** An API endpoint that takes a text input, generates a QR code from it, and returns the QR code as a Base64-encoded string, which can be easily used in web applications.
